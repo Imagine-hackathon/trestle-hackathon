42c35f00e683ca41f3c1f42e5e11a28948a3a189
isaac-svg <isaacsakyi009@gmail.com>
2024-07-30 17:35:09 +0000

    ADD Dashboard Page

--- src/app/dashboard/[adminId]/page.tsx
+++ src/app/dashboard/[adminId]/page.tsx
@@ -1,9 +1,421 @@
-import React from "react";
+"use client";
+import Link from "next/link";
+import {
+  Bell,
+  CircleUser,
+  Home,
+  Menu,
+  Package,
+  Package2,
+  Search,
+  ShoppingCart,
+  Users,
+} from "lucide-react";
 
-type Props = {};
+import {
+  Area,
+  AreaChart,
+  Bar,
+  BarChart,
+  CartesianGrid,
+  Label,
+  LabelList,
+  Line,
+  LineChart,
+  PolarAngleAxis,
+  RadialBar,
+  RadialBarChart,
+  Rectangle,
+  ReferenceLine,
+  XAxis,
+  YAxis,
+} from "recharts";
 
-const page = (props: Props) => {
-  return <div>page</div>;
-};
+import { CardFooter } from "@/components/ui/card";
+import {
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+import { Separator } from "@/components/ui/separator";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import jobAnimation from "@/animations/jobanimation.json";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Input } from "@/components/ui/input";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import Lottie from "lottie-react";
 
-export default page;
+export default function Dashboard() {
+  return (
+    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+      <div className="hidden border-r bg-muted/40 md:block">
+        <div className="flex h-full max-h-screen flex-col gap-2">
+          <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
+            <Link href="/" className="flex items-center gap-2 font-semibold">
+              <Package2 className="h-6 w-6" />
+              <span className="">Imagine</span>
+            </Link>
+            <Button variant="outline" size="icon" className="ml-auto h-8 w-8">
+              <Bell className="h-4 w-4" />
+              <span className="sr-only">Toggle notifications</span>
+            </Button>
+          </div>
+          <div className="flex-1">
+            <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
+              <Link
+                href="#"
+                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+              >
+                <Home className="h-4 w-4" />
+                Dashboard
+              </Link>
+              <Link
+                href="#"
+                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+              >
+                <ShoppingCart className="h-4 w-4" />
+                Post job
+                <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
+                  6
+                </Badge>
+              </Link>
+              <Link
+                href="#"
+                className="flex items-center gap-3 rounded-lg bg-muted px-3 py-2 text-primary transition-all hover:text-primary"
+              >
+                <Package className="h-4 w-4" />
+                Application{" "}
+              </Link>
+              {/* <Link
+                href="#"
+                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+              >
+                <Users className="h-4 w-4" />
+                Customers
+              </Link> */}
+              {/* <Link
+                href="#"
+                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+              >
+                <LineChart className="h-4 w-4" />
+                Analytics
+              </Link> */}
+            </nav>
+          </div>
+          {/* <div className="mt-auto p-4">
+            <Card x-chunk="dashboard-02-chunk-0">
+              <CardHeader className="p-2 pt-0 md:p-4">
+                <CardTitle>Upgrade to Pro</CardTitle>
+                <CardDescription>
+                  Unlock all features and get unlimited access to our support
+                  team.
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="p-2 pt-0 md:p-4 md:pt-0">
+                <Button size="sm" className="w-full">
+                  Upgrade
+                </Button>
+              </CardContent>
+            </Card>
+          </div> */}
+        </div>
+      </div>
+      <div className="flex flex-col">
+        <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button
+                variant="outline"
+                size="icon"
+                className="shrink-0 md:hidden"
+              >
+                <Menu className="h-5 w-5" />
+                <span className="sr-only">Toggle navigation menu</span>
+              </Button>
+            </SheetTrigger>
+            <SheetContent side="left" className="flex flex-col">
+              <nav className="grid gap-2 text-lg font-medium">
+                <Link
+                  href="#"
+                  className="flex items-center gap-2 text-lg font-semibold"
+                >
+                  <Package2 className="h-6 w-6" />
+                  <span className="sr-only">Imagine</span>
+                </Link>
+                <Link
+                  href="#"
+                  className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
+                >
+                  <Home className="h-5 w-5" />
+                  Dashboard
+                </Link>
+                <Link
+                  href="#"
+                  className="mx-[-0.65rem] flex items-center gap-4 rounded-xl bg-muted px-3 py-2 text-foreground hover:text-foreground"
+                >
+                  <ShoppingCart className="h-5 w-5" />
+                  Orders
+                  <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
+                    6
+                  </Badge>
+                </Link>
+                <Link
+                  href="#"
+                  className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
+                >
+                  <Package className="h-5 w-5" />
+                  Products
+                </Link>
+                <Link
+                  href="#"
+                  className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
+                >
+                  <Users className="h-5 w-5" />
+                  Customers
+                </Link>
+                <Link
+                  href="#"
+                  className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
+                >
+                  <LineChart className="h-5 w-5" />
+                  Analytics
+                </Link>
+              </nav>
+              <div className="mt-auto">
+                {/* <Card>
+                  <CardHeader>
+                    <CardTitle>Upgrade to Pro</CardTitle>
+                    <CardDescription>
+                      Unlock all features and get unlimited access to our
+                      support team.
+                    </CardDescription>
+                  </CardHeader>
+                  <CardContent>
+                    <Button size="sm" className="w-full">
+                      Upgrade
+                    </Button>
+                  </CardContent>
+                </Card> */}
+              </div>
+            </SheetContent>
+          </Sheet>
+          <div className="w-full flex-1">
+            <form>
+              <div className="relative">
+                <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+                <Input
+                  type="search"
+                  placeholder="Search products..."
+                  className="w-full appearance-none bg-background pl-8 shadow-none md:w-2/3 lg:w-1/3"
+                />
+              </div>
+            </form>
+          </div>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="secondary" size="icon" className="rounded-full">
+                <CircleUser className="h-5 w-5" />
+                <span className="sr-only">Toggle user menu</span>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Settings</DropdownMenuItem>
+              <DropdownMenuItem>Support</DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Logout</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </header>
+        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+          <div className="flex items-center">
+            <h1 className="text-lg font-semibold md:text-2xl">Dashboard</h1>
+          </div>
+          <div className=" bg-gray-800 p-2 sm:p-4 flex-col sm:flex-row flex justify-between items-center w-full h-fit sm:h-52 rounded-lg ">
+            <div className="flex flex-col flex-1  justify-between  h-full py-6 ">
+              <p className="text-white font-medium leading-8 text-3xl">
+                Welcome to Imagine
+              </p>
+              <h3 className="text-3xl font-extrabold text-white ">
+                Devine Sakyi
+              </h3>
+            </div>
+            <div className="flex-1 justify-end h-full w-56  flex items-center ">
+              <Lottie
+                animationData={jobAnimation}
+                loop={true}
+                className="w-56 sm:w-72 h-56"
+              />
+            </div>
+          </div>
+          <div className="">
+            <div className="flex ">
+              {/* 1 */}
+              <Card className="lg:max-w-md" x-chunk="charts-01-chunk-0">
+                <CardHeader className="space-y-0 pb-2">
+                  <CardDescription>Today</CardDescription>
+                  <CardTitle className="text-4xl tabular-nums">
+                    12,584{" "}
+                    <span className="font-sans text-sm font-normal tracking-normal text-muted-foreground">
+                      steps
+                    </span>
+                  </CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <ChartContainer
+                    config={{
+                      steps: {
+                        label: "Steps",
+                        color: "hsl(var(--chart-1))",
+                      },
+                    }}
+                  >
+                    <BarChart
+                      accessibilityLayer
+                      margin={{
+                        left: -4,
+                        right: -4,
+                      }}
+                      data={[
+                        {
+                          date: "2024-01-01",
+                          steps: 2000,
+                        },
+                        {
+                          date: "2024-01-02",
+                          steps: 2100,
+                        },
+                        {
+                          date: "2024-01-03",
+                          steps: 2200,
+                        },
+                        {
+                          date: "2024-01-04",
+                          steps: 1300,
+                        },
+                        {
+                          date: "2024-01-05",
+                          steps: 1400,
+                        },
+                        {
+                          date: "2024-01-06",
+                          steps: 2500,
+                        },
+                        {
+                          date: "2024-01-07",
+                          steps: 1600,
+                        },
+                      ]}
+                    >
+                      <Bar
+                        dataKey="steps"
+                        fill="var(--color-steps)"
+                        radius={5}
+                        fillOpacity={0.6}
+                        activeBar={<Rectangle fillOpacity={0.8} />}
+                      />
+                      <XAxis
+                        dataKey="date"
+                        tickLine={false}
+                        axisLine={false}
+                        tickMargin={4}
+                        tickFormatter={(value) => {
+                          return new Date(value).toLocaleDateString("en-US", {
+                            weekday: "short",
+                          });
+                        }}
+                      />
+                      <ChartTooltip
+                        defaultIndex={2}
+                        content={
+                          <ChartTooltipContent
+                            hideIndicator
+                            labelFormatter={(value) => {
+                              return new Date(value).toLocaleDateString(
+                                "en-US",
+                                {
+                                  day: "numeric",
+                                  month: "long",
+                                  year: "numeric",
+                                }
+                              );
+                            }}
+                          />
+                        }
+                        cursor={false}
+                      />
+                      <ReferenceLine
+                        y={1200}
+                        stroke="hsl(var(--muted-foreground))"
+                        strokeDasharray="3 3"
+                        strokeWidth={1}
+                      >
+                        <Label
+                          position="insideBottomLeft"
+                          value="Average Steps"
+                          offset={10}
+                          fill="hsl(var(--foreground))"
+                        />
+                        <Label
+                          position="insideTopLeft"
+                          value="12,343"
+                          className="text-lg"
+                          fill="hsl(var(--foreground))"
+                          offset={10}
+                          startOffset={100}
+                        />
+                      </ReferenceLine>
+                    </BarChart>
+                  </ChartContainer>
+                </CardContent>
+                <CardFooter className="flex-col items-start gap-1">
+                  <CardDescription>
+                    Over the past 7 days, you have walked{" "}
+                    <span className="font-medium text-foreground">53,305</span>{" "}
+                    steps.
+                  </CardDescription>
+                  <CardDescription>
+                    You need{" "}
+                    <span className="font-medium text-foreground">12,584</span>{" "}
+                    more steps to reach your goal.
+                  </CardDescription>
+                </CardFooter>
+              </Card>
+              {/* 2 */}
+            </div>
+          </div>
+          {/* <div
+            className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm"
+            x-chunk="dashboard-02-chunk-1"
+          >
+            <div className="flex flex-col items-center gap-1 text-center">
+              <h3 className="text-2xl font-bold tracking-tight">
+                You have no products
+              </h3>
+              <p className="text-sm text-muted-foreground">
+                You can start selling as soon as you add a product.
+              </p>
+              <Button className="mt-4">Add Product</Button>
+            </div>
+          </div> */}
+        </main>
+      </div>
+    </div>
+  );
+}
