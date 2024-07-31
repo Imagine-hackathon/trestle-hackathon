0aefc745355aae8cb913c44b74b53c7c21dc8f4f
ioenimil <isaacoboenimil@gmail.com>
2024-07-31 17:50:45 +0000

    removed unused code

--- src/app/api/create-embeddings/route.ts
+++ src/app/api/create-embeddings/route.ts
@@ -1,5 +1,3 @@
-// import "pdf-parse"; // Peer dep
-// import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
 
 import { pineconeIndex } from "@/lib/pinecone";
@@ -8,20 +6,20 @@ import { prepareDocument } from "@/lib/utils";
 import { convertToAscii, embedDocuments } from "@/lib/embedding";
 
 export async function GET(req: Request) {
-  const loader = new PDFLoader("src/app/api/create-embeddings/test.pdf");
+    const loader = new PDFLoader("src/app/api/create-embeddings/test.pdf");
 
-  const pages = (await loader.load()) as PDFPage[];
+    const pages = (await loader.load()) as PDFPage[];
 
-  //split and segment the docs
-  const documents = await Promise.all(pages.map(prepareDocument));
-  console.log(documents);
+    //split and segment the docs
+    const documents = await Promise.all(pages.map(prepareDocument));
+    console.log(documents);
 
-  const vectors = await Promise.all(documents.flat().map(embedDocuments));
-  const namespace = pineconeIndex.namespace("imaginec");
+    const vectors = await Promise.all(documents.flat().map(embedDocuments));
+    const namespace = pineconeIndex.namespace("imaginec");
 
-  // const namespace = pineconeIndex.namespace(convertToAscii(fileKey));
+    // const namespace = pineconeIndex.namespace(convertToAscii(fileKey));
 
-  await namespace.upsert(vectors);
+    await namespace.upsert(vectors);
 
-  return Response.json({ status: 200 });
+    return Response.json({ status: 200 });
 }
