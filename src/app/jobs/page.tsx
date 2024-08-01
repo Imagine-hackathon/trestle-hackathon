9dafcf3bc9282bdda239850c440454d7d2d446e1
William Adotei Allotey <william.a.allotey@gmail.com>
2024-08-01 09:33:52 +0000

    Update page.tsx

--- src/app/jobs/page.tsx
+++ src/app/jobs/page.tsx
@@ -216,71 +216,33 @@ const Jobs = () => {
     }
     if (jobList) {
       setFilteredJobs(
-        jobList?.filter((job: JobListingProps) => {
-          console.log(
-            filters,
-            mapJobExperience(job.experience),
-            filters.experienceLevel
-              .map((i) => i.toLowerCase())
-              .includes(mapJobExperience(job.experience).toLowerCase())
-          );
-          return (
-            (filters.jobType.length === 0 ||
-              filters.jobType
-                ?.map((i) => i.replace(" ", "").replace("-", "").toLowerCase())
-                .includes(
-                  job.type.toLowerCase().replace(" ", "").replace("-", "")
-                )) &&
-            (filters.experienceLevel.length === 0 ||
+        jobList
+          ?.filter((job: JobListingProps) => {
+            console.log(
+              filters,
+              mapJobExperience(job.experience),
               filters.experienceLevel
                 .map((i) => i.toLowerCase())
-                .includes(mapJobExperience(job.experience).toLowerCase())) &&
-            (filters.preferredLocation.length === 0 ||
-              filters.preferredLocation.includes("Both") ||
-              filters.preferredLocation
-                .map((e) => e.toLowerCase())
-                .includes(job.location.toLowerCase())) &&
-            (filters.location === "" ||
-              job.officelocation
-                .toLowerCase()
-                .includes(filters.location.toLowerCase())) &&
-            (filters.company === "" ||
-              job.company.toLowerCase().includes(filters.company.toLowerCase()))
-          );
-        }).sort((a, b)=>{
-          if (sortBy === null){
-            return 0
-          }
-          else {
-            if (sortBy === 'salary'){
-              return b.salary - a.salary
-            } else {
-              return b.timeCreated.seconds - a.timeCreated.seconds
-            }
-            }
-        })
-      );
-      return;
-    }
-    fetchAndReturn("jobs", "userId", "!=", "")
-      .then((data) => {
-        setJobList(data);
-        console.log(data);
-        setFilteredJobs(
-          data?.filter((job: JobListingProps) => {
-            console.log(filters);
+                .includes(mapJobExperience(job.experience).toLowerCase())
+            );
             return (
               (filters.jobType.length === 0 ||
-                filters.jobType.includes(job.type)) &&
+                filters.jobType
+                  ?.map((i) =>
+                    i.replace(" ", "").replace("-", "").toLowerCase()
+                  )
+                  .includes(
+                    job.type.toLowerCase().replace(" ", "").replace("-", "")
+                  )) &&
               (filters.experienceLevel.length === 0 ||
-                filters.experienceLevel.includes(
-                  mapJobExperience(job.experience)
-                )) &&
+                filters.experienceLevel
+                  .map((i) => i.toLowerCase())
+                  .includes(mapJobExperience(job.experience).toLowerCase())) &&
               (filters.preferredLocation.length === 0 ||
-                job.location.toLowerCase() === "both" ||
-                filters.preferredLocation.includes(
-                  job.location.toLowerCase()
-                )) &&
+                filters.preferredLocation.includes("Both") ||
+                filters.preferredLocation
+                  .map((e) => e.toLowerCase())
+                  .includes(job.location.toLowerCase())) &&
               (filters.location === "" ||
                 job.officelocation
                   .toLowerCase()
@@ -290,18 +252,62 @@ const Jobs = () => {
                   .toLowerCase()
                   .includes(filters.company.toLowerCase()))
             );
-          }).sort((a: JobListingProps, b: JobListingProps)=>{
-            if (sortBy === null){
-              return 0
-            }
-            else {
-              if (sortBy === 'salary'){
-                return b.salary - a.salary
-              } else {
-                return a.timeCreated.seconds - b.timeCreated.seconds
-              }
-              }
           })
+          .sort((a, b) => {
+            if (sortBy === null) {
+              return 0;
+            } else {
+              if (sortBy === "salary") {
+                return b.salary - a.salary;
+              } else {
+                return b.timeCreated.seconds - a.timeCreated.seconds;
+              }
+            }
+          })
+      );
+      return;
+    }
+    fetchAndReturn("jobs", "userId", "!=", "")
+      .then((data) => {
+        setJobList(data);
+        console.log(data);
+        setFilteredJobs(
+          data
+            ?.filter((job: JobListingProps) => {
+              console.log(filters);
+              return (
+                (filters.jobType.length === 0 ||
+                  filters.jobType.includes(job.type)) &&
+                (filters.experienceLevel.length === 0 ||
+                  filters.experienceLevel.includes(
+                    mapJobExperience(job.experience)
+                  )) &&
+                (filters.preferredLocation.length === 0 ||
+                  job.location.toLowerCase() === "both" ||
+                  filters.preferredLocation.includes(
+                    job.location.toLowerCase()
+                  )) &&
+                (filters.location === "" ||
+                  job.officelocation
+                    .toLowerCase()
+                    .includes(filters.location.toLowerCase())) &&
+                (filters.company === "" ||
+                  job.company
+                    .toLowerCase()
+                    .includes(filters.company.toLowerCase()))
+              );
+            })
+            .sort((a: JobListingProps, b: JobListingProps) => {
+              if (sortBy === null) {
+                return 0;
+              } else {
+                if (sortBy === "salary") {
+                  return b.salary - a.salary;
+                } else {
+                  return a.timeCreated.seconds - b.timeCreated.seconds;
+                }
+              }
+            })
         );
       })
       .catch((e: any) => console.log(e));
@@ -358,10 +364,35 @@ const Jobs = () => {
                 </div>
               </CardHeader>
               <CardContent>
-                <p className="text-sm text-gray-500 mb-4">
-                  Showing {filteredJobs?.length || 0} of {jobList?.length || 0}{" "}
-                  jobs
-                </p>
+                {filteredJobs !== undefined ? (
+                  <p className="text-sm text-gray-500 mb-4">
+                    Showing {filteredJobs?.length || 0} of{" "}
+                    {jobList?.length || 0} jobs
+                  </p>
+                ) : (
+                  <p>
+                    <svg
+                      className="mr-3 h-5 w-5 animate-spin dark:text-white text-gray-900"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                    >
+                      <circle
+                        className="opacity-25"
+                        cx="12"
+                        cy="12"
+                        r="10"
+                        stroke="currentColor"
+                        strokeWidth="4"
+                      ></circle>
+                      <path
+                        className="opacity-75"
+                        fill="currentColor"
+                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                      ></path>
+                    </svg>
+                  </p>
+                )}
                 <div className="space-y-4">
                   {filteredJobs?.map((job, key) => (
                     <JobListing key={key} {...job} />
